adf86346128e82c4f322ad113f30794e57336e42
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-12-25 21:24:27 -0500

    Home page draft, page directory skeleton

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import Footer from '@/components/footer';
+import NavBar from '@/components/navbar';
+import SocialSideBar from '@/components/socialsidebar';
+
+export default function Contact() {
+    return (
+        <main>
+            <NavBar/>
+            <SocialSideBar/>
+
+            <div>
+
+            </div>
+
+            <Footer/>
+        </main>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,77 @@
-import Image from 'next/image'
+import Footer from '@/components/footer';
+import NavBar from '@/components/navbar';
+import SocialSideBar from '@/components/socialsidebar';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen">
+      <NavBar/>
+      <SocialSideBar/>
+
+      {/* HERO */}
+      <div className="flex flex-col items-center bg-[url('/assets/bghomegraddark.png')] bg-center bg-cover bg-scroll h-[90vh]">
+        <div className='pt-96 subpixel-antialiased text-center'>
+          <p className='text-white font-librecaslontext font-bold text-[48px]'>
+            Tackling the overlooked healthcare
+          </p>
+          <p className='text-white font-librecaslontext font-bold text-[48px]'>
+            crisis of epilepsy in Africa.
+          </p>
+          <p className='pt-4 text-white font-librecaslontext font-bold text-[22px] hover:brightness-90 underline'>
+            <Link href='/about#what-is-epilepsy'>What is epilepsy?</Link>
+          </p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* SECTION 0 - SOCIAL MEDIA */}
+      <div className='bg-[#ffbdae] h-[10vh]'>
+        <p>SECTION 0</p>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* SECTION 1 - AFRICA'S BIGGEST DISORDER */}
+      <div className='justify-center items-center py-24 flex flex-col bg-white'>
+        <div className='flex justify-between items-center'>
+          <Image src='/assets/samplewebsitephoto1.png' width={512} height={512} alt=''/>
+          <div className='px-8 max-w-[780px] tracking-wide flex flex-col justify-between'>
+            <div className='pb-10 text-3xl text-red-600'>
+              <h1>Addressing Africa's <u>biggest</u> neurological disorder.</h1>
+            </div>
+            <p className='text-lg text-balance'>
+              Grounded Neurons is an initiative focused on tackling the greatly overlooked health crisis of epilepsy in Africa, a disorder that affects more than 25 million individuals
+              in Africa, with 80% living in low to middle-income countries.
+            </p>
+            <p className='pt-8 pb-2 text-lg text-balance'>
+              Epilepsy is a brain disorder where the brain experiences sudden, unpredictable bursts of electrical activity, commonly leading to seizures.
+            </p>
+            <p className='pt-4 text-black font-librecaslontext font-bold text-lg hover:brightness-90'>
+              <Link href='/about#what-is-epilepsy'><u>More About Epilepsy</u> &rarr;</Link>
+            </p>
+          </div>
+        </div>
       </div>
+
+      {/* SECTION 2 - PROFESSOR YOUTUBE VIDEO */}
+      <div className='py-24 flex justify-center items-center'>
+        <div className='px-8 max-w-[720px] tracking-wide text-lg text-balance'>
+          <div className='pb-10 text-3xl'>
+            <h1 className='pb-2 text-red-600'>A Scholar's Perspective:</h1>
+            <h1 className='text-slate-700'>Dr. Amadou Gallo Diop, University of Dakar</h1>
+          </div>
+          <p className=''>
+            Dr. Amadou Gallo Diop, a neurology and epileptology professor at the University of Dakar and the head of African Affairs for the International League Against Epilepsy,
+            discusses the status of epilepsy in Africa and the growing treatment disparity.
+          </p>
+        </div>
+        <div className='border-4 border-red-600'>
+          <iframe className='h-[25vh] w-[512px]' src="https://www.youtube-nocookie.com/embed/4q4I0HOVthI?si=r_s_J5_l2fhcMiMT" title="YouTube video player"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+          </iframe>
+        </div>
+      </div>
+
+      <Footer/>
     </main>
-  )
-}
+  );
+};
