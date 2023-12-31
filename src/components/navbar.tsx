bc86c02d705fb3dafcc17d1e64c23eebf005f344
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-12-31 12:06:59 -0500

    Navbar updates

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { FaBars } from 'react-icons/fa';
 import { GrClose } from 'react-icons/gr';
+import { IoMdArrowDropdown } from "react-icons/io";
 import { useEffect, useState } from 'react';
 
 import gnLogoFull from '/public/assets/GroundingNeurons.png';
@@ -29,8 +30,12 @@ const NavBar = ({page} : {page:string}) => {
     );
 
     return (
-        <nav className={`top-0 left-0 w-full z-[999] fixed ${page!=='home' ? 'bg-orange-800' : 'bg-orange-800/75'} ${isScrolled ? 'backdrop-blur-[10px]' : 'h-20 transition-all'} ${nav ? 'bg-zinc-900 md:bg-transparent transition-all' : 'transition-all'}`}>
-            <div className={`${isScrolled ? '' : 'pt-2 transition-all'} flex font-quicksand text-white justify-between items-center font-semibold max-w-[1240px] m-auto p-1`}>
+        <nav className={`top-0 left-0 h-20 w-full z-[999] absolute ${page!=='home' ? 'bg-orange-800' : 'bg-orange-800/75'}`}>
+            {/* MOBILE */}
+
+
+            {/* DESKTOP */}
+            <div className='pt-2 flex font-quicksand text-white justify-between items-center font-semibold max-w-[1240px] m-auto p-1'>
                 <ul className='flex items-center'>
                     <li className='pt-2 hover:brightness-95 transition-all'>
                         <Link href='/'>
@@ -38,24 +43,25 @@ const NavBar = ({page} : {page:string}) => {
                         </Link>
                     </li>
                 </ul>
-                <ul className='flex'>
+                <ul className='flex items-center justify-center'>
                     <li className='p-4'>
                         <Link href='/' className={`transition-all hover:brightness-90 relative hover-underline-animation`}>
                             Home
                         </Link>
                     </li>
-                    <li className='p-4'>
+                    <li className='flex items-center justify-center p-4'>
                         <Link href='/about' className={`transition-all hover:brightness-90 relative hover-underline-animation`}>
-                            About Us
+                            About Our Initiative
                         </Link>
+                        <IoMdArrowDropdown/>
                     </li>
-                    <li className='p-4'>
+                    <li className='flex items-center p-4'>
                         <Link href='/contact' className={`transition-all hover:brightness-90 relative hover-underline-animation`}>
-                            Contact
+                            Contact Us
                         </Link>
                     </li>
-                    <li className='p-4'>
-                        <Link href='/donate' className={`transition-all hover:brightness-90 relative hover-underline-animation`}>
+                    <li className='flex'>
+                        <Link href='/donate' className={`px-4 py-2 ml-3 rounded-lg outline outline-red-400 bg-red-400/30 hover:brightness-90 transition-all relative`}>
                             Donate
                         </Link>
                     </li>
