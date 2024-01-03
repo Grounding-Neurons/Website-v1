622f7affd1111688ddefa9f003489f6421e64f17
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2024-01-03 11:49:15 -0500

    Finished contact page, fully responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -19,7 +19,7 @@ export default function Contact() {
             <div id='triggerElement' className='pt-20 flex relative justify-center items-center h-[35vh]'>
                 <Image src={background} fill={true} priority={true} quality={100} alt='' className='z-0 object-cover object-center'/>
                 <div className='absolute z-1'>
-                    <p className='font-librecaslontext text-white text-3xl max-w-72 sm:max-w-none sm:text-4xl underlineunderline-offset-[14px] slide-animation-short text-center'>
+                    <p className='font-librecaslontext text-white text-3xl max-w-72 sm:max-w-none sm:text-4xl slide-animation-short text-center'>
                         Contact Grounding Neurons
                     </p>
                 </div>
@@ -32,13 +32,13 @@ export default function Contact() {
 
             {/* SECTION 2 - WEBMASTER CONTACT */}
             <div id='webmaster' className='flex flex-col sm:py-8 justify-center items-center'>
-                <div className='bg-gray-500 h-[2px] w-full max-w-sm sm:max-w-7xl'/>
+                <div className='bg-gray-500 h-[2px] w-[90vw] sm:max-w-7xl'/>
                 <div className='flex flex-col py-8 sm:py-12 justify-center items-center'>
                     <p className='font-librecaslontext text-red-800 text-3xl sm:text-4xl'>Webmaster Contact</p>
                 </div>
-                <div className='bg-gray-500 h-[2px] w-full max-w-sm sm:max-w-7xl'/>
+                <div className='bg-gray-500 h-[2px] w-[90vw] sm:max-w-7xl'/>
                 <div className='pt-10 sm:pt-16 text-center max-w-[1080px]'>
-                    <p className='font-librecaslontext text-gray-600 text-base sm:text-xl'>For any questions, concerns, or reports about the Grounding Neurons website, including any potential vulnerabilites or broken content, please contact the Webmaster at the email below:</p>
+                    <p className='sm:px-6 font-librecaslontext text-gray-600 text-base max-w-xs sm:max-w-none sm:text-xl'>For any questions, concerns, or reports about the Grounding Neurons website, including any potential vulnerabilites or broken content, please contact the Webmaster at the email below:</p>
                     <div className='pt-12'>
                         <button className='rounded-md bg-orange-800 px-3.5 py-2.5 text-center text-sm hover:bg-orange-700 transition-all'>
                             <Link href={'mailto:webmaster@groundingneurons.org'} className='flex items-center'>
